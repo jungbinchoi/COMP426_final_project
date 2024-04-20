da3db76a76ca05644658487b8974103836d5c889
Jungbin Choi <jung12@email.unc.edu>
2024-04-19 21:14:05 -0400

    Initialize App

---
+++ back-end/app.mjs
@@ -0,0 +1,16 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello, COMP 426!");
+});
+
+app.listen(port, () => {
+  console.log(`Running --- localhost:${port}/`);
+});
