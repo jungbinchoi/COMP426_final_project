844ccc5ef06c75fdbd1817de38c246299c8db1af
Jungbin Choi <jung12@email.unc.edu>
2024-04-23 01:43:29 -0400

    Implemented guess()

--- back-end/utils.mjs
+++ back-end/utils.mjs
@@ -1,1 +1,27 @@
 import { db } from "./db.mjs";
+
+export class Utils {
+  static #currWord = "";
+
+  static guess(word) {
+    if (word === undefined || typeof word !== "string") {
+      return { valid: false };
+    }
+
+    if (word === this.#currWord) {
+      return { correct: true, results: [] };
+    } else {
+      let spliced = word.split("");
+      let resultsArr = [];
+
+      spliced.map((char, i) => {
+        resultsArr.push({
+          inWord: word.includes(char),
+          inPlace: char === word[i],
+        });
+      });
+
+      return { correct: false, results: resultsArr };
+    }
+  }
+}
