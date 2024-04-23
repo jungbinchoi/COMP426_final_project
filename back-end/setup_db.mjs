6850e8c508489afdd2d7f17cb4b8f0ee122d25ae
Jungbin Choi <jung12@email.unc.edu>
2024-04-23 11:46:24 -0400

    Create RUNS and GAMES database
    Runs table records the unique ids of runs and the score earned for the run. The Games table M-to-O to Runs via Runs' id and records the words used

--- back-end/setup_db.mjs
+++ back-end/setup_db.mjs
@@ -1,3 +1,11 @@
 import { db } from "./db.mjs";
 
+await db.run(
+  "CREATE TABLE runs (id PRIMARY KEY AUTOINCREMENT, score INTEGER NOT NULL)"
+);
+
+await db.run(
+  "CREATE TABLE games (rid INTEGER NOT NULL, word TEXT NOT NULL, FOREIGN KEY (rid) REFERENCES runs(id))"
+);
+
 db.close();
