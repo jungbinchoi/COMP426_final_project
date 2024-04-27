ff802f643b025ee350287ae508a51bb9c8ecafbe
Jungbin Choi <jung12@email.unc.edu>
2024-04-26 21:02:57 -0400

    Create config.mjs

---
+++ back-end/config.mjs
@@ -0,0 +1,12 @@
+/* 
+    How to get a OpenAI API key:
+
+    1. Go to https://openai.com/ and log in
+    2. Choose "API"
+    3. On the main dashboard page, in the side bar that expands out, choose "API keys"
+    4. If no API keys are present, press "Create new secret key"
+    5. Name can be whatever, set rest to default
+    6. Copy the key and paste it below
+ */
+
+export const apiKey = "Your API Key";
