554fbe17d2afdffc060d03d062fda0370b171105
Jungbin Choi <jung12@email.unc.edu>
2024-04-27 21:28:24 -0400

    Changed 'scores' schema

--- back-end/setup_db.mjs
+++ back-end/setup_db.mjs
@@ -1,11 +1,13 @@
 import { db } from "./db.mjs";
 
 await db.run(
-  "CREATE TABLE scores (one INTEGER NOT NULL, two INTEGER NOT NULL, three INTEGER NOT NULL, four INTEGER NOT NULL, five INTEGER NOT NULL, six INTEGER NOT NULL)"
+  "CREATE TABLE scores (number INTEGER PRIMARY KEY, amount INTEGER NOT NULL)"
 );
 
 await db.run("CREATE TABLE words (word TEXT NOT NULL)");
 
-await db.run("INSERT INTO scores VALUES (0, 0, 0, 0, 0, 0)");
+await db.run(
+  "INSERT INTO scores VALUES (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0);"
+);
 
 db.close();
